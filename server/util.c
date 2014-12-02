7c00087b5e3f6b5892871d5dc3cbc8a547e97768
semitrivial <rmhiale@ucl.ac.uk>
2014-12-02 06:21:27 +0000

    Improve white space in third-party code

--- server/util.c
+++ server/util.c
@@ -131,21 +131,29 @@ char from_hex(char ch) {
   return isdigit(ch) ? ch - '0' : tolower(ch) - 'a' + 10;
 }
 
-char *url_decode(char *str) {
+char *url_decode(char *str)
+{
   char *pstr = str, *buf = malloc(strlen(str) + 1), *pbuf = buf;
-  while (*pstr) {
-    if (*pstr == '%') {
-      if (pstr[1] && pstr[2]) {
+
+  while (*pstr)
+  {
+    if (*pstr == '%')
+    {
+      if (pstr[1] && pstr[2])
+      {
         *pbuf++ = from_hex(pstr[1]) << 4 | from_hex(pstr[2]);
         pstr += 2;
       }
-    } else if (*pstr == '+') {
-      *pbuf++ = ' ';
-    } else {
-      *pbuf++ = *pstr;
     }
+    else
+    if (*pstr == '+')
+      *pbuf++ = ' ';
+    else
+      *pbuf++ = *pstr;
+
     pstr++;
   }
+
   *pbuf = '\0';
   return buf;
 }
